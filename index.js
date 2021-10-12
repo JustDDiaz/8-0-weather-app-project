fda79725a1b8fa298d4e0aa2a09badb11e38ee5d
justin <justin.d.diaz@gmail.com>
2021-10-12 12:01:27 -0400

    not done

--- index.js
+++ index.js
@@ -5,54 +5,68 @@ document.querySelector(".form form").addEventListener("submit", (event) => {
   fetch(`https://wttr.in/${search}?format=j1`)
     .then((response) => response.json())
     .then((weather) => {
-      if (!search.length) {
-        const resultArea = document.querySelector(".result p");
-        resultArea.textContent = `Enter Location.`;
-        document.querySelector(".result p").classList.add("error");
-        event.target.reset();
-      } else {
-        const resultDef = document.querySelector(".result p");
-        resultDef.textContent = ``;
-        const resultH2 = document.querySelector(".result h2");
-        resultH2.textContent = weather.nearest_area[0].areaName[0].value;
-        const resultArea = document.querySelector(".result .area");
-        resultArea.textContent = `Area: ${weather.nearest_area[0].areaName[0].value}`;
-        const resultReg = document.querySelector(".result .region");
-        resultReg.textContent = `Region: ${weather.nearest_area[0].region[0].value}`;
-        const resultCountry = document.querySelector(".result .country");
-        resultCountry.textContent = `Country: ${weather.nearest_area[0].country[0].value}`;
-        const resultCurr = document.querySelector(".result .currently");
-        resultCurr.textContent = `Currently: Feels like ${weather.current_condition[0].FeelsLikeF}°F`;
-        const today = document.querySelector(".today h3");
-        today.removeAttribute("hidden");
-        const todayAve = document.querySelector(".today .ave");
-        todayAve.textContent = `Average Temperature: ${weather.weather[0].avgtempF}°F`;
-        const todayMax = document.querySelector(".today .max");
-        todayMax.textContent = `Max Temperature ${weather.weather[0].maxtempF}°F`;
-        const todayMin = document.querySelector(".today .min");
-        todayMin.textContent = `Min Temperature ${weather.weather[0].mintempF}°F`;
-        const tommorrow = document.querySelector(".tommorrow h3");
-        tommorrow.removeAttribute("hidden");
-        const tommAve = document.querySelector(".tommorrow .ave");
-        tommAve.textContent = `Average Temperature: ${weather.weather[1].avgtempF}°F`;
-        const tommMax = document.querySelector(".tommorrow .max");
-        tommMax.textContent = `Max Temperature ${weather.weather[1].maxtempF}°F`;
-        const tommMin = document.querySelector(".tommorrow .min");
-        tommMin.textContent = `Min Temperature ${weather.weather[1].mintempF}°F`;
-        const after = document.querySelector(".after h3");
-        after.removeAttribute("hidden");
-        const aftAve = document.querySelector(".after .ave");
-        aftAve.textContent = `Average Temperature: ${weather.weather[2].avgtempF}°F`;
-        const aftMax = document.querySelector(".after .max");
-        aftMax.textContent = `Max Temperature ${weather.weather[2].maxtempF}°F`;
-        const aftMin = document.querySelector(".after .min");
-        aftMin.textContent = `Min Temperature ${weather.weather[2].mintempF}°F`;
-        const prevSer = document.querySelector(".history ul");
-        const li = document.createElement("li");
-        li.textContent = `${weather.nearest_area[0].areaName[0].value} - ${weather.current_condition[0].FeelsLikeF}°F`;
-        prevSer.append(li);
-        event.target.reset();
-      }
+      // if (!search.length) {
+      //   const resultArea = document.querySelector(".result p");
+      //   resultArea.textContent = `Enter Location.`;
+      //   document.querySelector(".result p").classList.add("error");
+      //   event.target.reset();
+      // } else {
+      const resultDef = document.querySelector(".result p");
+      resultDef.textContent = ``;
+      const resultH2 = document.querySelector(".result h2");
+      resultH2.textContent = weather.nearest_area[0].areaName[0].value;
+      const resultArea = document.querySelector(".result .area");
+      resultArea.textContent = `Area: ${weather.nearest_area[0].areaName[0].value}`;
+      const resultReg = document.querySelector(".result .region");
+      resultReg.textContent = `Region: ${weather.nearest_area[0].region[0].value}`;
+      const resultCountry = document.querySelector(".result .country");
+      resultCountry.textContent = `Country: ${weather.nearest_area[0].country[0].value}`;
+      const resultCurr = document.querySelector(".result .currently");
+      resultCurr.textContent = `Currently: Feels like ${weather.current_condition[0].FeelsLikeF}°F`;
+      const today = document.querySelector(".today h3");
+      today.removeAttribute("hidden");
+      const todayAve = document.querySelector(".today .ave");
+      todayAve.textContent = `Average Temperature: ${weather.weather[0].avgtempF}°F`;
+      const todayMax = document.querySelector(".today .max");
+      todayMax.textContent = `Max Temperature ${weather.weather[0].maxtempF}°F`;
+      const todayMin = document.querySelector(".today .min");
+      todayMin.textContent = `Min Temperature ${weather.weather[0].mintempF}°F`;
+      const tommorrow = document.querySelector(".tommorrow h3");
+      tommorrow.removeAttribute("hidden");
+      const tommAve = document.querySelector(".tommorrow .ave");
+      tommAve.textContent = `Average Temperature: ${weather.weather[1].avgtempF}°F`;
+      const tommMax = document.querySelector(".tommorrow .max");
+      tommMax.textContent = `Max Temperature ${weather.weather[1].maxtempF}°F`;
+      const tommMin = document.querySelector(".tommorrow .min");
+      tommMin.textContent = `Min Temperature ${weather.weather[1].mintempF}°F`;
+      const after = document.querySelector(".after h3");
+      after.removeAttribute("hidden");
+      const aftAve = document.querySelector(".after .ave");
+      aftAve.textContent = `Average Temperature: ${weather.weather[2].avgtempF}°F`;
+      const aftMax = document.querySelector(".after .max");
+      aftMax.textContent = `Max Temperature ${weather.weather[2].maxtempF}°F`;
+      const aftMin = document.querySelector(".after .min");
+      aftMin.textContent = `Min Temperature ${weather.weather[2].mintempF}°F`;
+      const prevSer = document.querySelector(".history ul");
+      const li = document.createElement("li");
+      li.textContent = ` - ${weather.current_condition[0].FeelsLikeF}°F`;
+      const link = document.createTextNode(
+        `${weather.nearest_area[0].areaName[0].value}`
+      );
+      const a = document.createElement("a");
+      a.append(link);
+      a.setAttribute("href", `https://wttr.in/${search}?format=j1`);
+      prevSer.append(li);
+      document
+        .querySelector(".history li")
+        .addEventListener("click", (event) => {
+          event.preventDefault();
+        });
+      li.prepend(a);
+      const text = document.querySelector(".history p");
+      text.textContent = "";
+      event.target.reset();
+      //}
       console.log(weather);
     });
 });
